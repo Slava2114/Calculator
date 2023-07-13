a6f51b20e43e2257556966b3e06a020f4f2e0d19
Slava2114 <pro100yarik1243@yandex.ru>
2023-07-13 22:02:07 +0300

    calculator na massive

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,28 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int  operation;
-        double  firstValue, secondValue, result = 0d;
+        double result = 0d;
+        double[] values = new double[2];
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter first value");
-        firstValue = scanner.nextInt();
+        values[0] = scanner.nextDouble();
         System.out.println("Enter second value");
-        secondValue = scanner.nextInt();
+        values[1] = scanner.nextDouble();
         System.out.println("Enter operation");
-        operation = scanner.nextInt();
+        int operation = scanner.nextInt();
         if (operation == 1) {
-            result = firstValue + secondValue;
+            result = values[0] + values[1];
+        } else if (operation == 2) {
+            result = values[0] - values[1];
+        } else if (operation == 3) {
+            result = values[0] * values[1];
+        } else if (operation == 4) {
+            result = values[0] / values[1];
+        } else if (operation == 5) {
+            result = values[0] / values[1] * 100;
+        } else {
+            System.out.println("Incorrect operation");
         }
-        else if (operation == 2) {
-            result = firstValue - secondValue;
-        }
-        else if (operation == 3) {
-            result = firstValue * secondValue;
-        }
-        else if (operation == 4) {
-            result = firstValue / secondValue;
-        }
-        else System.out.println("Incorrect operation");
 
         System.out.println("The result is " + result);
 
